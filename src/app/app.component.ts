0478e6fc33d958853ef80f341eaa514021e6305a
Quan Truong <qmtruong92@gmail.com>
2018-01-06 09:29:54 -0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  // onSubmit(form: NgForm) {
+  //   // console.log('submitted!');
+  //   // console.log(form);
+  //   // console.log(form.value);
+  //   console.log('Username: ' + form.value.username + ' has submitted a form.');
+  // }
+
+  onSubmit() {
+
+    // Print the NgForm to the console
+    console.log(this.signupForm);
+
+    // Display the username entered in the form
+    console.log(this.signupForm.value.username);
+
+    // Check whether the form is valid. The form is valid if the email is entered correctly.
+    console.log(this.signupForm.valid ? 'Valid' : 'Not Valid');
+  }
+}
